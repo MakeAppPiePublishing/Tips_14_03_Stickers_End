5d139fcd6e56add1620e582587b797b51a08c278
Steven Lipton <steve@makeapppie.com>
2021-01-29 09:42:52 -0600

    Initial Commit

---
+++ PizzaTime/Stages.swift
@@ -0,0 +1,16 @@
+//
+//  Stages.swift
+//  WidgetDemo
+//
+//  Created by Steven Lipton on 10/23/20.
+//
+// An exercise file for iOS Development Tips Weekly
+// A weekly course on LinkedIn Learning for iOS developers
+//  Season 14 (Q2 2021) video 03
+//  by Steven Lipton (C)2020, All rights reserved
+// Learn more at https://linkedin-learning.pxf.io/YxZgj
+//  For more code, go to http://bit.ly/AppPieGithub
+
+import Foundation
+
+let stages = ["Order a Pizza", "Pizza Ordered", "Making Pizza","Cooking Pizza","Delivery Pickup","On its way","Delivered","Enjoy"]
